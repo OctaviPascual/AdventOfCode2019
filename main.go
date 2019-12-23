8c556b12f2a5b37136222bd506e85c4cfcdd884c
Octavi Pascual Sarda <octavi.pascualsarda@datadoghq.com>
2019-12-23 13:36:29 +0100

    Remove endline at the end of the input file

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/OctaviPascual/AdventOfCode2019/day01"
 )
 
 func main() {
 	bytes, _ := ioutil.ReadFile("./day01/day01.txt")
-	day := day01.NewDay(string(bytes))
+	input := string(bytes)
+	input = strings.TrimSuffix(input, "\n")
+	day := day01.NewDay(input)
 	fmt.Println(day.SolvePartOne().Format())
 	fmt.Println(day.SolvePartTwo().Format())
 }
